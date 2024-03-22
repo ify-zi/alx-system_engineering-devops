dc05d88f49b6df2f79efa273740034f62749b62a
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-03-22 15:06:51 -0700

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using puppet to install a package
 
-package { 'Installing python package':
+exec { 'Installing python package':
   command =>  'pip3 install flask',
   path    =>  '/usr/bin',
   unless  =>  '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py',
