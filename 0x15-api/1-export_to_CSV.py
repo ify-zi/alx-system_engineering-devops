90d61fa46c06a98aa3c4e4fe1fa37a92e5df577d
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-22 09:09:08 -0700

    export csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,11 +16,6 @@ if __name__ == '__main__':
     todos_info = requests.get(test_url + "todos",
                               params={'user_id': user_id}).json()
 
-    tasks = [t.get('title') for t in todos_info if t.get('completed') is True]
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user_info.get("name"), len(tasks), len(todos_info)))
-    [print("\t {}".format(i)) for i in tasks]
-
     with open("{}.csv".format(user_id), 'w', encoding='utf-8') as exp:
         writer = csv.writer(exp, quoting=csv.QUOTE_ALL)
         [writer.writerow(
@@ -28,4 +23,4 @@ if __name__ == '__main__':
                           user_info.get("username"),
                           t.get("completed"),
                           t.get("title")]
-                         ) for t in todos_info]
+                         ) for t in todos_info[:20]]
