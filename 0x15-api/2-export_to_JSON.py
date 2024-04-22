ae3d58c36213ad3eb3c7c9de3ad2b3fa064eefdd
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-22 14:37:43 -0700

    exort to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    API request modules
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    test_url = 'https://jsonplaceholder.typicode.com/'
+    user_id = sys.argv[1]
+    user_info = requests.get(test_url + "users/{}".format(user_id)).json()
+    todos_info = requests.get(test_url + "todos",
+                              params={'user_id': user_id}).json()
+    dict_values = {user_id: [
+                             {'title': todo.get('title'),
+                              'completed': todo.get('completed'),
+                              'username': user_info.get('username')
+                             } for todo in todos_info]}
+    with open("{}.json".format(user_id), "w", newline="") as j_file:
+        json.dump(dict_values, j_file)
