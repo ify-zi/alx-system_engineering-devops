1773c4d0ae19d9c80aea5483ea5029190c749cea
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-22 08:52:32 -0700

    export files to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
           .format(user_info.get("name"), len(tasks), len(todos_info)))
     [print("\t {}".format(i)) for i in tasks]
 
-    with open("USER_ID.csv", 'w', encoding='utf-8') as exp:
+    with open("{}.csv".format(user_id), 'w', encoding='utf-8') as exp:
         writer = csv.writer(exp, quoting=csv.QUOTE_ALL)
         [writer.writerow(
                          [user_id,
