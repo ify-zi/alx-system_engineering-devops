f563b18b59fc00cc3d4b5e5d99b86c335d3b00cc
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-05-07 13:39:28 -0700

    debugging

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-    Reddit api
-"""
+"""Reddit api"""
 
 
 def number_of_subscribers(subreddit):
-    """function returns tthe count of subscribers"""
+    """Queries the Reddit API 
+        and returns the number of subscribers"""
     import requests
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-
-    resp = requests.get(url,
-                        headers=headers,
-                        allow_redirects=False)
-    if resp.status_code >= 300:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-    return resp.json().get("data").get("subscribers")
+    return sub_info.json().get("data").get("subscribers")
