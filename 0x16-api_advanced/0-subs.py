3c553697098c15248affee6f2f13c3eaaa6d0bc3
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-05-07 12:56:28 -0700

    get number of subsrcibers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Reddit api
+"""
+
+
+
+def number_of_subscribers(subreddit):
+    """function returns tthe count of subscribers"""
+    import requests
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    resp = requests.get(url,
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+    if resp.status_code >= 300:
+        return None
+
+    return resp.json().get("data").get("subscribers")
